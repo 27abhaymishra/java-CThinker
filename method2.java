e9d025524a25f24608515fc02c7745a9446a5089
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-10 15:26:13 +0530

    Add files via upload

---
+++ method2.java
@@ -0,0 +1,11 @@
+public class method2 {
+    public static void main(String[] args) {
+        int a =5;
+        show(a);
+    }
+    public static void show(int x){
+        System.out.println(x);
+    }
+}
+
+
