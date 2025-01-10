11ca5e22d6e9bda6b25d8c1bb7ba14810d7eed66
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-10 15:25:55 +0530

    Add files via upload

---
+++ method1.java
@@ -0,0 +1,33 @@
+public class method1 {
+
+    public static  void main(String[] args) {
+        int a = 6;
+   
+        // display(a);
+        // System.out.println(a);
+      
+        // int arr [] = {1,2,3,4};
+        // display(arr);
+        // System.out.println(arr[0]);
+        System.out.println(display(a));
+        System.out.println(a);
+
+    }
+
+    public  static int display(int x){
+        // System.out.println("Display Hu");
+    //   a = "aman";
+        //   arr[0] = 6;
+        // System.out.println(a);
+        // x = 7;
+        // return x;
+        if(x == 4){
+            return 7;
+          
+        }else{
+            return -1;
+        }
+
+    }
+    
+}
