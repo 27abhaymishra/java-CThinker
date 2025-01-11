d2462b0baf7d3a4fa237d82416879e96db1959bb
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-11 15:02:47 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,49 @@
+public class Bitwise {
+    public static void main(String[] args) {
+    //    int a = 6 , b = 3;
+    //    a = a ^b;
+
+    //    b = a^b;
+    //    a =a^b;
+    //     // System.out.println(a&b);
+    //     // System.out.println(a|b);
+    //     // System.out.println(a^b);
+    //     // System.out.println(a<<b);
+    //     // System.out.println(a>>b);
+    //     System.out.println(a);
+    //     System.out.println(b);  
+    //     System.out.println(a^b);
+
+
+    // int num = 11;   
+    //  System.out.println((num % 2 == 0 ) ? "Number is even" : "Number is not a even ");
+    //  int a = 11 , b = 7; // int x = 015;
+    //   System.out.println(3.53f == 3.53);  
+    //   System.out.println(3 == 3.0);
+    //   System.out.println(96.4f > 96.4);
+    //   System.out.println(3.0f > 3.0);
+    //   System.out.println(x);
+    //   System.out.println(0x1f);
+    // System.out.println((a>b)? a : b);
+
+    // int n = 3 ;
+    // int m = 6;
+    // int temp ;
+    // temp = n;
+    // n = m;
+    // m = temp;
+    // n = n ^m;
+    // m = n ^m;
+    // n = n ^m;
+    // System.out.println(n);
+    // System.out.println(m);
+int num = 16;
+// System.out.println((num & (num-1) )== 0);
+System.out.println(Integer.bitCount(num)==1);
+
+
+
+
+    
+    }
+}
