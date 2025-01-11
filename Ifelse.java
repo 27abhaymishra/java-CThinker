78feb29651fcacc5bc71a79b2a1716d4ad4b1376
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-11 15:02:19 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,22 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        if (5 > 3) {
+            System.out.println(1);
+        }
+        if (3 > 2) {
+            System.out.println(2);
+        }
+
+        if (5 >= 4) {
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
+
+        if (5 > 3)
+            System.out.println(1);
+        else
+            System.out.println(2);
+    }
+
+}
