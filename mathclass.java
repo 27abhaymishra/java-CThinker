b7f49d48dca0aeeafb679ce7fbc3e6cc08e643e5
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-12 14:46:35 +0530

    Add files via upload

---
+++ mathclass.java
@@ -0,0 +1,11 @@
+public class mathclass {
+  public static void main(String[] args) {
+    int a = 6,b=3,c=8;
+    System.out.println(Math.max(a,b));
+    System.out.println(Math.max(a,Math.max(b,c)));
+    System.out.println(Math.sqrt(100));
+    System.out.println(Math.pow(2, 3));
+    System.out.println(Math.abs(-5));
+    System.out.println(Math.random());
+  }  
+}
