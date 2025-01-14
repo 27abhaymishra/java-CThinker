e7d5f5138ac89d70364ea02d70ef9969ef1f5346
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-14 14:23:54 +0530

    Add files via upload

---
+++ Ap.java
@@ -0,0 +1,9 @@
+public class Ap {
+    public static void main(String[] args) {
+        int x =8 , y = 2, z =5;
+        if((y-x) == (z-y)){
+            System.out.println("0");
+        }
+        else if()
+    }
+}
