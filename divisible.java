f2af277a7c9183717a7beecd8e8781350813705c
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-01-14 14:24:35 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,10 @@
+public class divisible {
+   public static void main(String[] args) {
+    int n = 8;
+    if((n % 3 == 0) && (n % 5 == 0)){
+        System.out.println("Number is divisible by 3 and 5");
+    }else {
+        System.out.println("No");
+    }
+   } 
+}
