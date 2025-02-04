6d92315b5615063a05b5b3f9b17f40d31ca21c84
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-02-04 14:50:44 +0530

    Add files via upload

---
+++ Loops/print.java
@@ -0,0 +1,14 @@
+public class print {
+
+    public static void main(String[] args) {
+        char i = 'A' ;
+        while(i<='Z'){
+            if(i!='A' && i !='E' && i!='I' && i!='O' && i!='U'){
+                System.out.println(i);
+            }
+
+            i++;
+          
+        }
+    }
+}
